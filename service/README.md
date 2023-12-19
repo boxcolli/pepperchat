4156f6e4e6a3581e28c8a876bb82802e956ba085
yoonmin <dmg04158@gmail.com>
2023-12-19 15:02:18 +0900

    simplified

--- service/README.md
+++ service/README.md
@@ -1,1 +1,4 @@
 # /service
+
+## MongoDB
+docker run -e MONGO_INITDB_ROOT_USERNAME=root -e MONGO_INITDB_ROOT_PASSWORD=root -p 27017:27017 mongo
--- service/cmd/server/main.go
+++ service/cmd/server/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/boxcolli/pepperchat/service/internal/docrepo"
@@ -24,78 +23,6 @@ func main() {
 		}
 	}
     app := fiber.New()
-
-    // GET /api/register
-    app.Get("/api/*", func(c *fiber.Ctx) error {
-        msg := fmt.Sprintf("✋ %s", c.Params("*"))
-        return c.SendString(msg) // => ✋ register
-    })
-
-    // GET /flights/LAX-SFO
-    app.Get("/flights/:from-:to", func(c *fiber.Ctx) error {
-        msg := fmt.Sprintf("💸 From: %s, To: %s", c.Params("from"), c.Params("to"))
-        return c.SendString(msg) // => 💸 From: LAX, To: SFO
-    })
-
-    // GET /dictionary.txt
-    app.Get("/:file.:ext", func(c *fiber.Ctx) error {
-        msg := fmt.Sprintf("📃 %s.%s", c.Params("file"), c.Params("ext"))
-        return c.SendString(msg) // => 📃 dictionary.txt
-    })
-
-    // GET /john/75
-    app.Get("/:name/:age/:gender?", func(c *fiber.Ctx) error {
-        msg := fmt.Sprintf("👴 %s is %s years old", c.Params("name"), c.Params("age"))
-        return c.SendString(msg) // => 👴 john is 75 years old
-    })
-
-    // GET /john
-    app.Get("/:name", func(c *fiber.Ctx) error {
-        msg := fmt.Sprintf("Hello, %s 👋!", c.Params("name"))
-        return c.SendString(msg) // => Hello john 👋!
-    })
-
-	// POST /auth/signup/{username}
-	app.Post("/auth/signup/:username", func(c *fiber.Ctx) error {
-		id, err := dr.SignUp(c.Context(), c.Params("username"))
-
-		switch err {
-		case docrepo.ErrConflict:
-			return c.SendStatus(fiber.StatusConflict)
-		case docrepo.ErrUnknown:
-			return c.SendStatus(fiber.StatusInternalServerError)
-		default:
-			return c.SendString(id)
-		}
-	})
-
-	// GET /auth/signin/{username} => user_id
-	app.Get("/auth/signin/:username", func(c *fiber.Ctx) error {
-		id, err := dr.SignIn(c.Context(), c.Params("username"))
-
-		switch err {
-		case docrepo.ErrUnknown:
-			return c.SendStatus(fiber.StatusInternalServerError)
-		case docrepo.ErrNotFound:
-			return c.SendStatus(fiber.StatusNotFound)
-		default:
-			return c.SendString(id)
-		}
-	})
-
-	// GET /user/{user_id} => username
-	app.Get("/user/:user_id", func(c *fiber.Ctx) error {
-		name, err := dr.GetUser(c.Context(), c.Params("user_id"))
-
-		switch err {
-		case docrepo.ErrUnknown:
-			return c.SendStatus(fiber.StatusInternalServerError)
-		case docrepo.ErrNotFound:
-			return c.SendStatus(fiber.StatusNotFound)
-		default:
-			return c.SendString(name)
-		}
-	})
 
 	// POST /chat/{chatname} => chat_id
 	app.Post("/chat/:chatname", func(c *fiber.Ctx) error {
